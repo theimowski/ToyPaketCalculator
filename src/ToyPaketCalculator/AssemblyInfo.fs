1c083a25c553287730c85bac22deb048e079bd15
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-05-19 20:22:12 +0200

    Bump version to 0.0.1-beta

--- src/ToyPaketCalculator/AssemblyInfo.fs
+++ src/ToyPaketCalculator/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("ToyPaketCalculator")>]
 [<assembly: AssemblyProductAttribute("ToyPaketCalculator")>]
 [<assembly: AssemblyDescriptionAttribute("Toy Calculator project in F# for the purpose of demonstating paket.local feature")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
-    let [<Literal>] InformationalVersion = "1.0"
+    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] InformationalVersion = "0.0.1"
